4dfbd52ea17d9805e0ea8c96a2effbe7e545448e
Nathan Nutter <iam@nnutter.com>
2011-09-16 23:08:09 -0500

    added bare templates

---
+++ PwdHash.m
@@ -0,0 +1,23 @@
+//
+//  PwdHash.m
+//  PwdHash
+//
+//  Created by Nathan Nutter on 9/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PwdHash.h"
+
+@implementation PwdHash
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end

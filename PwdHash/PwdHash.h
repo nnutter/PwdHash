9acdb560eec0099c98a84688d5fcd7f0d8ae3438
Nathan Nutter <iam@nnutter.com>
2011-09-17 13:08:27 -0500

    combined several prior classes into PwdHash class

--- PwdHash/PwdHash.h
+++ PwdHash/PwdHash.h
@@ -8,9 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
-@interface PwdHash : NSObject
+@interface PwdHash : NSObject {
+	
+	NSString *urlString;
+	NSString *pwdString;
+	NSString *extras;
+	NSString *result;
+    
+}
 
-+ (NSString*)generate:(NSString*)password
-                  url:(NSString*)url;
+@property (retain) NSString* pwdString;
+@property (retain) NSString* urlString;
+
++ (NSString*)getHashedPasswordWithPasswordAndURL:(NSString*)thePassword
+                                             url:(NSString*)theURL;
+
+- (id) initWithPasswordAndURL:(NSString *)thePassword url:(NSString *)theURL;
++ (PwdHash *)PwdHashWithPasswordAndURL:(NSString *)thePassword pwd:(NSString *)theURL;
++ (NSString *)extractDomainFromURL:(NSString *)theURL;
+- (NSString *)applyConstraints:(NSString *)theString size:(NSUInteger)theSize;
+- (NSString *)getHashedPassword;
++ (NSString *)stringByMatching:(NSString *) string toRegexPattern:(NSString *)regexPattern;
+
+// Base64 Methods
++ (NSData *)decodeBase64WithString:(NSString *)strBase64;
++ (NSString *)encodeBase64WithData:(NSData *)objData;
++ (NSString *)encodeBase64WithString:(NSString *)strData;
+
+// MD5 Methods
++ (NSData *)calculateHMACMD5WithKey:(NSString *)key andData:(NSString *)data;
++ (NSString *)b64_hmac_md5:(NSString *)password realm:(NSString *)realm;
++ (NSString *)hex_hmac_md5:(NSString *)password realm:(NSString *)realm;
 
 @end
